6528bd24e59b2520c5b86232435e5643f6690f6b
Darren Shepherd <darren@rancher.com>
2020-09-23 16:03:50 -0700

    asdf

--- download-secret.sh
+++ download-secret.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-cat > runners/secret.yaml << EOF
-kind: Secret
-apiVersion: v1
+kubectl apply -f - << EOF
+kind: Bundle
+apiVersion: fleet.cattle.io/v1alpha1
 metadata:
-  name: fleet-agent-bootstrap
-  namespace: fleet-simulator
-data:
-  values: "$(kubectl -n simulator-clusters get secret token -o 'jsonpath={.data.values}')"
+    name: simulator-token
+    namespace: simulator-runners
+spec:
+  resources:
+  - content: |
+        kind: Secret
+        apiVersion: v1
+        metadata:
+        name: fleet-agent-bootstrap
+        namespace: fleet-simulator
+        data:
+        values: "$(kubectl -n simulator-clusters get secret token -o 'jsonpath={.data.values}')"
+  targets:
+  - clusterSelector: {}
 EOF
