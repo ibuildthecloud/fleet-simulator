a46ac2c3c438325ccebed470fe5b579c0faf1255
Darren Shepherd <darren@rancher.com>
2020-09-28 14:39:13 -0700

    1000

--- download-secret.sh
+++ download-secret.sh
@@ -19,3 +19,23 @@ spec:
   targets:
   - clusterSelector: {}
 EOF
+
+kubectl apply -f - << EOF
+kind: Bundle
+apiVersion: fleet.cattle.io/v1alpha1
+metadata:
+    name: simulator-token
+    namespace: fleet-default
+spec:
+  resources:
+  - content: |
+        kind: Secret
+        apiVersion: v1
+        metadata:
+          name: fleet-agent-bootstrap
+          namespace: fleet-simulator
+        data:
+          values: "$(kubectl -n fleet-default get secret token -o 'jsonpath={.data.values}')"
+  targets:
+  - clusterSelector: {}
+EOF
