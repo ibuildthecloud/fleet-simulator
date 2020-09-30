ee97de41be2e538cc7c2153a2241c454589515f0
Darren Shepherd <darren@rancher.com>
2020-09-30 11:49:29 -0700

    work

---
+++ clusters/k3s.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+>k3s.yaml
+
+for i in $(seq 0 100); do
+    cat >> k3s.yaml << EOF
+kind: K3s
+apiVersion: k3s.ibtc.io/v1
+metadata:
+  name: simulator-$i
+
+---
+kind: Cluster
+apiVersion: rancher.cattle.io/v1alpha1
+metadata:
+  name: simulator-$i
+  labels:
+    workspace: workspace-$i
+spec:
+  importedConfig:
+    kubeConfigSecret: simulator-$i-kubeconfig
+EOF
+
+done
