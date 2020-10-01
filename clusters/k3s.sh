4c4a1d9cc2d57c11386adeb3bd0c188ebfe86fa9
Darren Shepherd <darren@rancher.com>
2020-09-30 23:28:26 -0700

    work

--- clusters/k3s.sh
+++ clusters/k3s.sh
@@ -2,26 +2,28 @@
 
 >k3s.yaml
 
-for i in $(seq 1 2); do
-    cat >> k3s.yaml << EOF
+for i in $(seq -f '%05g' 1 2); do
+    for j in $(seq 1 2); do
+        cat >> k3s.yaml << EOF
 ---
 kind: K3s
 apiVersion: k3s.ibtc.io/v1
 metadata:
-  name: simulator-$i
+  name: simulator-${i}-${j}
   namespace: fleet-default
 
 ---
 kind: Cluster
 apiVersion: rancher.cattle.io/v1
 metadata:
-  name: simulator-$i
+  name: simulator-${i}-${j}
   namespace: fleet-default
   labels:
-    workspace: workspace-$i
+    workspace: "workspace-$i"
+    count: "$j"
 spec:
   importedConfig:
-    kubeConfigSecret: simulator-$i-kubeconfig
+    kubeConfigSecret: simulator-${i}-${j}-kubeconfig
 EOF
-
+    done
 done
